69d171e1cd2fe2d41dc8c1274f486985e045feca
s1avo <s.nadolny@hotmail.com>
2025-10-19 19:50:53 +0200

    feat: add parseXML utility to parse CxF XML using fast-xml-parser

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { XMLParser } from "fast-xml-parser";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export const parseXML = <T>(xml: string): T => {
+	const parser = new XMLParser({
+		ignoreAttributes: false,
+		attributeNamePrefix: "@_",
+		allowBooleanAttributes: true,
+		parseAttributeValue: true,
+		parseTagValue: true,
+		trimValues: true,
+		isArray: (tagName) => tagName === "cc:ReflectanceSpectrum",
+	});
+
+	return parser.parse(xml) as T;
+};
