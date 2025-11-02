bb2474bcecc4e348ad9ddf155475bb181fcb34ff
s1avo <s.nadolny@hotmail.com>
2025-11-02 16:04:17 +0100

    feat: add chromatic adaptation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { XMLParser } from "fast-xml-parser";
+import { type Illuminants, type Observers } from "@/lib/types";
+import { multiply, diag, pinv, type Matrix } from "mathjs";
+import { ADAPTATIONS, ILLUMINANTS } from "@/lib/constant";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
@@ -19,3 +22,37 @@ export const parseXML = <T>(xml: string): T => {
 
 	return parser.parse(xml) as T;
 };
+
+/** Tworzy macierz adaptacji barwnej */
+function getAdaptationMatrix(wpSrc: Illuminants, wpDst: Illuminants, observer: Observers) {
+	const mSharp = ADAPTATIONS["bradford"] as Matrix;
+
+	const wpSrcArr = ILLUMINANTS[observer][wpSrc.toLowerCase() as "d50" | "d65"];
+	const wpDstArr = ILLUMINANTS[observer][wpDst.toLowerCase() as "d50" | "d65"];
+
+	const rgbSrc = multiply(mSharp, wpSrcArr).toArray() as [number, number, number];
+	const rgbDst = multiply(mSharp, wpDstArr).toArray() as [number, number, number];
+
+	const ratio = rgbDst.map((v, i) => v / rgbSrc[i]!);
+	const mRat = diag(ratio);
+
+	const mSharpInv = pinv(mSharp);
+
+	return multiply(multiply(mSharpInv, mRat), mSharp);
+}
+
+/** Adaptacja barwna pojedynczego punktu XYZ */
+export function applyChromaticAdaptation(
+	valX: number,
+	valY: number,
+	valZ: number,
+	origIllum: Illuminants,
+	targIllum: Illuminants,
+	observer: Observers = "2"
+) {
+	const transformMatrix = getAdaptationMatrix(origIllum, targIllum, observer);
+
+	const XYZ = [valX, valY, valZ];
+
+	return multiply(transformMatrix, XYZ).toArray() as [number, number, number];
+}
