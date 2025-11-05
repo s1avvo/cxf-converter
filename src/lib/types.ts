4161f71de1995d45e7d840f56fb493a0074f7eb4
s1avo <s.nadolny@hotmail.com>
2025-11-05 20:16:22 +0100

    ref: refine CxF types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,8 +1,7 @@
 export type ReflectanceSpectrum = {
-	"@_ColorSpecification": string;
-	"@_MeasureDate"?: string;
 	"@_Name": string;
 	"@_StartWL": number;
+	"@_ColorSpecification": string;
 	"#text": string;
 };
 
@@ -11,7 +10,7 @@ export type ColorSpecification = {
 	"cc:TristimulusSpec": {
 		"cc:Illuminant": string;
 		"cc:Observer": string;
-		"cc:Method"?: string;
+		"cc:Method": string;
 	};
 	"cc:MeasurementSpec"?: {
 		"cc:MeasurementType"?: string;
@@ -21,10 +20,11 @@ export type ColorSpecification = {
 			"@_StartWL": number;
 		};
 		"cc:CalibrationStandard"?: string;
-		"cc:Aperture"?: string;
 		"cc:Device"?: {
 			"cc:Model"?: string;
+			"cc:SerialNumber"?: number;
 			"cc:DeviceFilter"?: string;
+			"cc:DeviceIllumination"?: string | boolean;
 			"cc:DevicePolarization"?: string | boolean;
 		};
 	};
@@ -47,7 +47,7 @@ export type CxFFile = {
 					"@_GUID": string;
 					"@_Id": string;
 					"@_Name": string;
-					"@_ObjectType": string;
+					"@_ObjectType": "Target" | "Substrate" | "Standard" | string;
 					"cc:ColorValues": {
 						"cc:ReflectanceSpectrum": ReflectanceSpectrum[];
 					};
@@ -66,6 +66,7 @@ export type Illuminants = "d50" | "d65";
 
 export type ParsedSpectrum = {
 	id: string;
+	name: string;
 	illuminant: Illuminants;
 	observer: Observers;
 	spectrum: number[];
