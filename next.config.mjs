400547faee2ff4751a4576557a444c03f273e092
s1avo <s.nadolny@hotmail.com>
2025-11-09 15:49:44 +0100

    ref: shift away from eslint/prettier to biome

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+	reactStrictMode: true,
+
+	reactCompiler: true,
+
+	experimental: {
+		typedEnv: true,
+	},
+};
+
+export default nextConfig;
--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-	reactStrictMode: true,
-
-	reactCompiler: true,
-};
-
-export default nextConfig;
