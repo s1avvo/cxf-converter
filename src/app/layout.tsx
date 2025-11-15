036bb016453336bfa6113a3289bd18441592547e
s1avo <s.nadolny@hotmail.com>
2025-11-15 16:45:14 +0100

    fix: improve loading fallback

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Footer } from "@ui/footer";
 import { Header } from "@ui/header";
 import { Toaster } from "@ui/shadcn/sonner";
+import { LoaderIcon } from "lucide-react";
 import { Suspense } from "react";
 import { ConverterProvider } from "@/context/convert-provider";
 
@@ -18,6 +19,16 @@ export const metadata: Metadata = {
 	description: "Convert CxF to CMYK, sRGB, OKLab, OKLch and HEX formats.",
 };
 
+function Loading() {
+	return (
+		<div className="container flex items-center justify-center min-h-[calc(100vh-4rem)] ">
+			<div className="p-2 rounded-full w-10 h-10" aria-description="Loading">
+				<LoaderIcon className="w-6 h-6" />
+			</div>
+		</div>
+	);
+}
+
 export default function RootLayout({
 	children,
 }: Readonly<{
@@ -27,13 +38,7 @@ export default function RootLayout({
 		<html lang="en" className={`${dmSans.variable} antialiased`}>
 			<body className="font-dm-sans min-h-full flex-col">
 				<Header />
-				<Suspense
-					fallback={
-						<div className="flex items-center justify-center min-h-screen">
-							<div className="text-lg">Loading...</div>
-						</div>
-					}
-				>
+				<Suspense fallback={<Loading />}>
 					<ConverterProvider>{children}</ConverterProvider>
 				</Suspense>
 				<Footer />
